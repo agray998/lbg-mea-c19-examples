54f965ef37d09a93a39f232e6701557304f881d4
agray <agray@qa.com>
2024-06-04 10:58:06 +0100

    made loop in exercise.sh more useful

--- exercise.sh
+++ exercise.sh
@@ -2,16 +2,15 @@
 make_files() {
 	dir_name=$1
 	files=$2
-	content1=$3
-	content2=$4
 	cd $dir_name
 	pwd
 	for file in ${files[@]}; do
 		touch $file
+		cat - >> $file <<EOF
+This is $file
+EOF
 	done
-	#echo $content1 > $file1
-	#echo $content2 > $file2
-	#cat $file1 $file2
+	for file in ${files[@]}; do cat $file; done
 }
 start_dir=$(pwd)
 folder1=$1
@@ -20,15 +19,11 @@ declare -a files=(file1.txt file2.txt)
 echo $start_dir
 mkdir $folder1
 make_files $folder1 \
-	   ${files[@]} \
-           "This is the first file" \
-           "This is the second file"
+	   ${files[@]}
 mkdir $folder2
 ls
 make_files $folder2 \
-	   ${files[@]} \
-	   "This file is in $folder2" \
-	   "This file is also in $folder2"
+	   ${files[@]}
 rm -rf $folder2
 echo "This is another file" > file3.txt
 cd $start_dir
