a0561f87c93aea6e6c492b9a373180babbce6d57
agray <agray@qa.com>
2024-06-04 08:53:05 +0100

    Initial Commit

---
+++ first_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+pwd
+ls
+echo "Hello from this bash script"
+echo "$0"
+echo "$1"
+echo "$2"
+mkdir $1
---
+++ heredoc-example.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cat - >> file.txt <<EOF
+line1
+line2
+line3
+line4
+...
+EOF
